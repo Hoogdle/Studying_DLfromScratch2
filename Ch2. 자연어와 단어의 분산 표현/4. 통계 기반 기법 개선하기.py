34bef3d4298b61a920da98bfeeef1ae6f1835ac2
Taeyeong Kim <xodud7737@gmail.com>
2024-03-16 18:00:59 +0900

    Studying

--- Ch2. 자연어와 단어의 분산 표현/4. 통계 기반 기법 개선하기.py
+++ Ch2. 자연어와 단어의 분산 표현/4. 통계 기반 기법 개선하기.py
@@ -114,4 +114,134 @@ print(W)
 # 이렇게 되면 U의 열벡터(단어의 기저 벡터)가 소실되게 되는것! == 차원감소
 
 
+### SVD 차원감소 구현
+# SVD는 넘파이의 linalg 모듈을 사용해 구현 가능(linalg는 선형대수의 약어)
 
+import sys
+sys.path.append('C:\\Users\\rlaxo\\Desktop\\deepscratch')
+import numpy as np
+import matplotlib.pyplot as plt
+from common.util import preprocess, create_co_matrix, ppmi
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+vocab_size = len(id_to_word)
+C = create_co_matrix(corpus,vocab_size)
+W = ppmi(C)
+
+# SVD
+U,S,V = np.linalg.svd(W)
+
+print(C[0]) #[0 1 0 0 0 0 0]
+print(W[0]) #[0.        1.8073549 0.        0.        0.        0.        0.       ]
+print(U[0]) #SVD 자체, 밀집벡터로 변환됨. #[-1.1102230e-16  3.4094876e-01 -1.2051624e-01 -3.8857806e-16  0.0000000e+00 -9.3232495e-01  8.7683712e-17]
+
+# 벡터의 차원을 감소시키려면 just 원하는 만큼만 뽑으면 됨.
+print(U[0][:2]) #[-1.1102230e-16  3.4094876e-01] # 2개만 뽑음
+
+# 그래프로 나타내기
+for word, word_id in word_to_id.items():
+        plt.annotate(word, (U[word_id,0],U[word_id,1])) #x,y 지점(벡터, U[word_id,0]과 U[word_id,1]이 나타내는 벡터)에 word에 담긴 텍스트를 그리도록 함
+
+plt.scatter(U[:,0],U[:,1],alpha=0.5) # U 행렬 중 (전체열 and 0행)과 (전채행 and 1행)를 그래프에 표시함. 투명도는 0.5
+plt.show() # 그래프 나타남
+
+# 지금은 데이터셋이 작아서 합당하지 않은 결과가 나오지만 앞으로는 PTB라는 큰 말뭉치를 사용하여 실험 수행
+
+
+### PTB(Penn Treebank) 데이터셋
+# PTB는 말뭉치중 하나
+# 희소한 단어는 <unk>로 치환 (unknown)
+# 구체적인 숫자는 N으로 치환
+# <eos> 문장의 끝
+
+
+# PTB 사용
+import sys
+sys.path.append('C:\\Users\\rlaxo\\Desktop\\deepscratch')
+from dataset import ptb
+
+corpus, word_to_id, id_to_word = ptb.load_data('train') #train,valid,test의 값을 줄 수 있음
+
+print('말뭉치의 크기 :',len(corpus))
+print(f'corpus[:30] : {corpus[:30]}')
+print()
+print(f'id_to_word[0] : {id_to_word[0]}')
+print(f'id_to_word[1] : {id_to_word[1]}')
+print(f'id_to_word[2] : {id_to_word[2]}')
+print()
+print(f'word_to_id["car"] : {word_to_id["car"]}')
+print(f'word_to_id["happy"] : {word_to_id["happy"]}')
+print(f'word_to_id["lexus"] : {word_to_id["lexus"]}')
+
+# 말뭉치의 크기 : 929589
+# corpus[:30] : [ 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+#  24 25 26 27 28 29]
+
+# id_to_word[0] : aer
+# id_to_word[1] : banknote
+# id_to_word[2] : berlitz
+
+# word_to_id["car"] : 3856
+# word_to_id["happy"] : 4428
+# word_to_id["lexus"] : 7426
+
+
+
+# ptb 데이터로 벡터만들고 평가하기
+
+import sys
+sys.path.append('C:\\Users\\rlaxo\\Desktop\\deepscratch')
+import numpy as np
+from common.util import most_similar, create_co_matrix, ppmi
+from dataset import ptb
+
+window_size = 2
+wordvec_size = 100
+
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+vocab_size = len(word_to_id)
+
+C = create_co_matrix(corpus,vocab_size,window_size)
+W = ppmi(C, verbose=True)
+
+try:
+    from sklearn.utils.extmath import randomized_svd #for 빠른 연산
+    U,S,V = randomized_svd(W, n_components=wordvec_size, n_iter=5,random_state=None)
+except ImportError:
+    U,S,V = np.linalg.svd(W) #위에꺼 안 되면 어쩔 수 없이 느린 연산
+
+word_vecs = U[:,:wordvec_size]
+
+querys = ['you','year','car','toyota']
+for query in querys:
+    most_similar(query,word_to_id,id_to_word,word_vecs,top=5)
+
+
+# [query] you
+#  i: 0.7016294002532959
+#  we: 0.6388040781021118
+#  anybody: 0.5868049263954163
+#  do: 0.5612815022468567
+#  'll: 0.512611985206604
+
+# [query] year
+#  month: 0.6957005262374878
+#  quarter: 0.691483736038208
+#  earlier: 0.6661214232444763
+#  last: 0.6327787041664124
+#  third: 0.6230477094650269
+
+# [query] car
+#  luxury: 0.6767407655715942
+#  auto: 0.6339930295944214
+#  vehicle: 0.597271203994751
+#  cars: 0.5888376235961914
+#  truck: 0.5693157911300659
+
+# [query] toyota
+#  motor: 0.7481387853622437
+#  nissan: 0.7147319912910461
+#  motors: 0.6946365833282471
+#  lexus: 0.6553674936294556
+#  honda: 0.6343469619750977
