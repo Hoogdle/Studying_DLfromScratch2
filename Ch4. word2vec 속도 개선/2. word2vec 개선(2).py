09e6f39f7064f780d3635f81717e4a27dccc3e64
Taeyeong Kim <xodud7737@gmail.com>
2024-03-18 20:59:53 +0900

    Studying

--- Ch4. word2vec 속도 개선/2. word2vec 개선(2).py
+++ Ch4. word2vec 속도 개선/2. word2vec 개선(2).py
@@ -221,3 +221,53 @@ class CBOW:
 
 
 ### CBOW 모델 학습 코드
+import sys
+sys.path.append('...')
+import numpy as np
+from common import config
+
+# GPU 사용을 위한 코드
+config.GPU = True
+
+import pickle
+from common.trainer import Trainer
+from common.optimizer import Adam
+
+from cbow import CBOW
+from common.util import create_contexts_target, tp_cpu, to_gpu
+from dataset import ptb
+
+# 하이퍼 파라미터 설정
+window_size = 5
+hidden_size = 100
+batch_size = 100
+max_epoch = 10
+
+# 데이터 읽기
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+vocab_size = len(word_to_id)
+
+contexts, target = create_contexts_target(corpus,window_size)
+if config.GPU:
+    contexts,target = to_gpu(contexts), to_gpu(target)
+
+# 모델 생성
+model = CBOW(vocab_size,hidden_size,window_size,corpus)
+optimizer = Adam()
+trainer = Trainer(model,optimizer)
+
+# 학습 시작
+trainer.fit(contexts,target,max_epoch,batch_size)
+trainer.plot()
+
+# 나중에 사용할 수 있도록 필요한 데이터 저장
+word_vecs = model.word.vecs
+if config.GPU:
+    word_vecs = to_gpu(word_vecs)
+parmas = {}
+params['word_vecs'] = word_vecs.astype(np.float16)
+params['word_to_id'] = word_to_id
+params['word_to_id'] = id_to_word
+pkl_file = 'cbow_params.pkl'
+with open(pkl_file,'wb') as f:
+    pickle.dump(parmas,f,-1)
