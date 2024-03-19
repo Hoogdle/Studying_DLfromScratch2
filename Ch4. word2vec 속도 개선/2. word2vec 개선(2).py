908b5f2aba2fe881f6267b7f3b5efb3f191191ff
Taeyeong Kim <xodud7737@gmail.com>
2024-03-19 14:17:12 +0900

    Studying

--- Ch4. word2vec 속도 개선/2. word2vec 개선(2).py
+++ Ch4. word2vec 속도 개선/2. word2vec 개선(2).py
@@ -271,3 +271,24 @@ params['word_to_id'] = id_to_word
 pkl_file = 'cbow_params.pkl'
 with open(pkl_file,'wb') as f:
     pickle.dump(parmas,f,-1)
+
+
+
+### ¸ðµ¨ Æò°¡
+import sys
+sys.path.append('C:\\Users\\rlaxo\\Desktop\\deepscratch')
+from common.util import most_similar
+import pickle
+
+pkl_file = 'cbow_params.pkl'
+
+with open(pkl_file,'rb') as f:
+    params = pickle.load(f)
+    word_vecs = params['word_vecs']
+    word_to_id = params['word_to_id']
+    id_to_word = params['id_to_word']
+
+querys = ['you','year','car','toyota']
+for query in querys:
+    most_similar(query,word_to_id,id_to_word,word_vecs,top=5)
+
